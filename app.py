e22d9b6e37312e75e177308f33271e30a3263984
prerak-patel <emailprerak@gmail.com>
2020-04-14 14:39:00 -0500

    List available routes

--- app.py
+++ app.py
@@ -8,9 +8,15 @@ app = Flask(__name__)
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
-
+    return(f"Welcome to Climate App!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+)
+    
 
 @app.route("/api/v1.0/precipitation")
 def getPrecipitationByDate():
